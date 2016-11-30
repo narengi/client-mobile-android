58205bb0251a1dffd334c84f27a752b55647d7f4
Behdad Abedi <behdad.222@gmail.com>
2016-11-30 13:25:25 +0330

    add validation in step 2

--- app/src/main/java/xyz/narengi/android/ui/fragment/HouseMapEntryFragment.java
+++ app/src/main/java/xyz/narengi/android/ui/fragment/HouseMapEntryFragment.java
@@ -22,6 +22,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.byagowi.persiancalendar.Utils;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -320,4 +321,13 @@ public class HouseMapEntryFragment extends HouseEntryBaseFragment implements OnM
         geoPoint.setLng(latLng.longitude);
         getHouse().setPosition(geoPoint);
     }
+
+	@Override
+	protected boolean validate() {
+		if (getHouse().getPosition() == null) {
+			Toast.makeText(getContext(), R.string.invalid_position, Toast.LENGTH_LONG).show();
+			return false;
+		}
+		return true;
+	}
 }
