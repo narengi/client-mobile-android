021965cb95deb0a423c85beefd82d9a6ff660eee
Behdad Abedi <behdad.222@gmail.com>
2016-12-02 17:14:14 +0330

    set token in picasso dowloader

--- app/src/main/java/xyz/narengi/android/ui/NarengiApplication.java
+++ app/src/main/java/xyz/narengi/android/ui/NarengiApplication.java
@@ -4,7 +4,19 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.squareup.okhttp.Interceptor;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Downloader;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
 import net.danlew.android.joda.JodaTimeAndroid;
+
+import java.io.IOException;
+
+import xyz.narengi.android.common.dto.AccountProfile;
 
 /**
  * @author Siavash Mahmoudpour
@@ -18,6 +30,24 @@ public class NarengiApplication extends Application {
         super.onCreate();
         instance = this;
         JodaTimeAndroid.init(this);
+
+        final String authorizationJsonHeader = AccountProfile.getLoggedInAccountProfile(this).getToken().getAuthString();
+        OkHttpClient picassoClient = new OkHttpClient();
+        picassoClient.networkInterceptors().add(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("access-token", authorizationJsonHeader)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        });
+
+        Picasso.setSingletonInstance(
+                new Picasso
+                        .Builder(this)
+                        .downloader(new OkHttpDownloader(picassoClient))
+                        .build());
     }
 
     @Override
