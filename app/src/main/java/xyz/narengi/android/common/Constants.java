ef41a5eef950ca60cfba148d39aed5ab0b9cad3b
Behdad Abedi <behdad.222@gmail.com>
2016-12-09 20:09:15 +0330

    change url

--- app/src/main/java/xyz/narengi/android/common/Constants.java
+++ app/src/main/java/xyz/narengi/android/common/Constants.java
@@ -5,7 +5,7 @@ package xyz.narengi.android.common;
  */
 public class Constants {
 
-    public static final String SERVER_BASE_URL = "http://149.202.20.233";
+    public static final String SERVER_BASE_URL = "http://api.narengi.xyz";
     public static final int CALENDAR_MONTHS_LIMIT = 1200;
 
 //    public static final String SERVER_BASE_URL = "http://192.99.109.86:3500/api/v1/";
